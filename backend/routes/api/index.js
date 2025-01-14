81fb3abad77bf2baa850bd9c9d0af24fe454d422
muhammadelshareif <melshareif1@gmail.com>
2025-01-13 20:33:07 -0500

    reducer

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,34 +1,39 @@
 const router = require("express").Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js');
-const bookingsRouter =require('./bookings.js');
-const reviewsRouter =require('./reviews.js');
-const spotImagesRouter =require('./spotImages.js');
-const reviewImagesRouter =require('./reviewImages.js');
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
+const bookingsRouter = require("./bookings.js");
+const reviewsRouter = require("./reviews.js");
+const spotImagesRouter = require("./spotImages.js");
+const reviewImagesRouter = require("./reviewImages.js");
 
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
-  // If current user session is valid, set req.user to the user in the database
-  // If current user session is not valid, set req.user to null
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
-router.use('/session', sessionRouter);
+router.use("/session", sessionRouter);
 
-router.use('/users', usersRouter);
+router.use("/users", usersRouter);
 
-router.use('/spots', spotsRouter);
+router.use("/spots", spotsRouter);
 
-router.use('/bookings', bookingsRouter);
+router.use("/bookings", bookingsRouter);
 
-router.use('/reviews', reviewsRouter);
+router.use("/reviews", reviewsRouter);
 
-router.use('/spot-images', spotImagesRouter);
+router.use("/spot-images", spotImagesRouter);
 
-router.use('/review-images', reviewImagesRouter);
+router.use("/review-images", reviewImagesRouter);
 
-router.post('/test', (req, res) => {
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
+// Test route for CSRF
+router.post("/api/test", function (req, res) {
   res.json({ requestBody: req.body });
 });
 
