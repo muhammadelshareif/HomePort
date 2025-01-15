a0e9267c8829066f1d0d8c621a17f35893131e15
muhammadelshareif <melshareif1@gmail.com>
2025-01-15 13:50:25 -0500

    updateforrender

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,30 @@
-const config = require('./index');
+// const config = require('./index');
+
+// module.exports = {
+//   development: {
+//     storage: config.dbFile,
+//     dialect: "sqlite",
+//     seederStorage: "sequelize",
+//     logQueryParameters: true,
+//     typeValidation: true
+//   },
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//     dialect: 'postgres',
+//     seederStorage: 'sequelize',
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false
+//       }
+//     },
+//     define: {
+//       schema: process.env.SCHEMA
+//     }
+//   }
+// };
+
+const config = require("./index");
 
 module.exports = {
   development: {
@@ -6,20 +32,27 @@ module.exports = {
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
-    typeValidation: true
+    typeValidation: true,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    seederStorage: 'sequelize',
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    seederStorage: "sequelize",
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: false
-      }
+        rejectUnauthorized: false,
+      },
     },
     define: {
-      schema: process.env.SCHEMA
-    }
-  }
+      schema: process.env.SCHEMA,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    logging: false, // Disable logging in production
+  },
 };
