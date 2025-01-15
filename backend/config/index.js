c70552dbf7eaaf3a57c372e40f62f55d1eb83ce5
muhammadelshareif <melshareif1@gmail.com>
2025-01-15 12:49:36 -0500

    updateforrender

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,19 @@
+// module.exports = {
+//     environment: process.env.NODE_ENV || 'development',
+//     port: process.env.PORT || 8000,
+//     dbFile: process.env.DB_FILE,
+//     jwtConfig: {
+//       secret: process.env.JWT_SECRET,
+//       expiresIn: process.env.JWT_EXPIRES_IN
+//     }
+//   };
+
 module.exports = {
-    environment: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 8000,
-    dbFile: process.env.DB_FILE,
-    jwtConfig: {
-      secret: process.env.JWT_SECRET,
-      expiresIn: process.env.JWT_EXPIRES_IN
-    }
-  };
+  environment: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 8000,
+  dbFile: process.env.DB_FILE || "db/dev.db", // Provide a default value
+  jwtConfig: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  },
+};
