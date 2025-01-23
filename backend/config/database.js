585e899f47c43cb8c4e367e539fc8ec04bf91e61
muhammadelshareif <melshareif1@gmail.com>
2025-01-23 12:31:07 -0500

    createspot

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,18 +10,21 @@ module.exports = {
     seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true,
+    schema: "lodging_schema",
   },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     seederStorage: "sequelize",
+    schema: "lodging_schema",
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
+      // Add schema to search path
+      searchPath: ["lodging_schema"],
     },
-
     pool: {
       max: 5,
       min: 0,
